055d5623850eaa110a3e6add8e2526cb8bbfb402
swanandisarpale <swanandisarpale@gmail.com>
2026-05-14 21:40:53 +0530

    added file

---
+++ const.js
@@ -0,0 +1,20 @@
+// You can create a constant array:
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars);
+// You can change an element:
+cars[0] = "Toyota";
+console.log(cars);
+// You can add an element:
+cars.push("Audi");
+console.log(cars);
+
+
+// You can create a const object:
+const car = {type:"Fiat", model:"500", color:"white"};
+console.log(car);
+// You can change a property:
+car.color = "red";
+console.log(car);
+// You can add a property:
+car.owner = "Johnson";
+console.log(car);
