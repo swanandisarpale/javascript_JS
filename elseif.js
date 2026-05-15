01f66454e1ec01818c3dd36179abbdb3da81a94e
swanandisarpale <swanandisarpale@gmail.com>
2026-05-15 18:09:42 +0530

    added file

---
+++ elseif.js
@@ -0,0 +1,10 @@
+let marks=70;
+if (marks>=90){
+    console.log("a grade");
+}else if(marks>=80){
+    console.log("b grade");
+}else if(marks>=70){
+    console.log("c grade");
+}else{
+    console.log("fail");
+}
