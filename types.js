6d29f2dd5e24afa7bbb76facb24f018559f6b33d
swanandisarpale <swanandisarpale@gmail.com>
2026-05-17 18:18:56 +0530

    added file

---
+++ types.js
@@ -0,0 +1,46 @@
+//String--A text of characters enclosed in quotes
+//Number--A number representing a mathematical value
+//Bigint--	A number representing a large integer
+//Boolean--	A data type representing true or false
+//Object--	A collection of key-value pairs of data
+//Undefined--A primitive variable with no assigned value
+//Null--A primitive value representing object absence
+//Symbol--	A unique and primitive identifier
+
+
+// String
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Number
+let length = 16;
+let weight = 7.5;
+
+// BigInt
+let x = 1234567890123456789012345n;
+let y = BigInt(1234567890123456789012345)
+
+// Boolean
+let x = true;
+let y = false;
+
+// Object
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object
+const date = new Date("2022-03-25");
+
+// Undefined
+let x;
+let y;
+
+// Null
+let x = null;
+let y = null;
+
+// Symbol
+const x = Symbol();
+const y = Symbol();
