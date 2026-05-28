8174b68510e478c27a24ddfc6aa6e5e61e4b1e58
swanandisarpale <swanandisarpale@gmail.com>
2026-05-28 21:26:31 +0530

    added file

---
+++ star_dimond.js
@@ -0,0 +1,29 @@
+let n = 5;
+
+for (let i = 1; i <= n; i++) {
+    let row = "";
+
+    for (let j = 1; j <= n - i; j++) {
+        row += " ";
+    }
+
+    for (let k = 1; k <= (2 * i - 1); k++) {
+        row += "*";
+    }
+
+    console.log(row);
+}
+
+for (let i = n - 1; i >= 1; i--) {
+    let row = "";
+
+    for (let j = 1; j <= n - i; j++) {
+        row += " ";
+    }
+
+    for (let k = 1; k <= (2 * i - 1); k++) {
+        row += "*";
+    }
+
+    console.log(row);
+}
