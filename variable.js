07ca28eeb7bd40e4930875d27500b63e111742a2
swanandisarpale <swanandisarpale@gmail.com>
2026-09-10 23:13:12 +0530

    added file

---
+++ variable.js
@@ -0,0 +1,24 @@
+let a=10;
+const apple=5; //let,var and const keywoards create variables
+console.log(a);
+console.log(apple);
+
+let x;
+x=20;
+console.log(x);
+
+console.log("swanandi"+" "+"sarpale"); //swanandi sarpale
+
+let person = "John Doe", carName = "Volvo", price = 200;
+var s=3+2+4;
+console.log(s);
+
+
+function myfunction() {
+  var p = 1;
+  let q= 2;
+  const r= 3;
+}
+//p can NOT be used again
+//q can NOT be used again
+//r can NOT be used again
